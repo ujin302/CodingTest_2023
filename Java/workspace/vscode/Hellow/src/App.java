f9ede81bcbdc5f81047dc4be462a5162a2af2809
ujin302 <ujin302@gmail.com>
2023-11-12 17:45:22 +0900

    백준 2단계 조건문 완료 백준 3단계 반복문 시작

--- Java/workspace/vscode/Hellow/src/App.java
+++ Java/workspace/vscode/Hellow/src/App.java
@@ -2,11 +2,60 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
-        Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
-        int b = sc.nextInt();
+        // 백준 2525 오븐시계
+        // Scanner sc = new Scanner(System.in);
+        // int h = sc.nextInt();
+        // int m = sc.nextInt();
+        // int time = sc.nextInt();
 
-        System.out.println(a + b);
+        // m += time;
+        // if (m >= 60) {
+        // h += m / 60;
+        // m -= 60 * (m / 60);
+        // if (h >= 24) {
+        // h -= 24;
+        // }
+        // }
+        // System.out.println(h + " " + m);
+
+        // 백준 2480 주사위 세개
+        // 같은 눈 3개 > 10000 + a *1000
+        // 같은 눈 2개 > 1000 + a *100
+        // 모두 다른 눈 > 가장 큰거 * 100
+        // Scanner sc = new Scanner(System.in);
+        // int a = sc.nextInt();
+        // int b = sc.nextInt();
+        // int c = sc.nextInt();
+        // int result = 0;
+        // if (a == b && b == c && c == a) {
+        // result = 10000 + a * 1000;
+        // } else if (a != b && b != c && c != a) {
+        // int max = a;
+        // if (a > b) {
+        // if (c > a) {
+        // max = c;
+        // }else {max = a;}
+        // } else if (b > c) {
+        // if (a > b) {
+        // max = a;
+        // }else {max = b;}
+        // } else if (c > a) {
+        // if (b > c) {
+        // max = b;
+        // }else {max = c;}
+        // }
+        // result = max * 100;
+        // } else {
+        // int num = 0;
+        // if (a == b && a != c) {
+        // num = a;
+        // } else if (a == c && a != b) {
+        // num = a;
+        // } else if (b == c && b != a) {
+        // num = b;
+        // }
+        // result = 1000 + num * 100;
+        // }
+        // System.out.println(result);
     }
 }
--- Java/workspace/vscode/Hellow/src/Main.java
+++ Java/workspace/vscode/Hellow/src/Main.java
@@ -3,12 +3,17 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
-        String b = sc.next();
+        int index = sc.nextInt();
+        int[][] ArrNum = new int[index][2];
 
-        System.out.println(a * (b.charAt(0) - '2'));
-        System.out.println(a * (b.charAt(0) - '1'));
-        System.out.println(a * (b.charAt(0) - '0'));
-        System.out.println(a * Integer.parseInt(b));
+        for (int i = 0; i < index; i++) {
+            for (int j = 0; j < 2; j++) {
+                ArrNum[i][j] = sc.nextInt();
+            }
+        }
+
+        for (int i = 0; i < index; i++) {
+            System.out.println(ArrNum[i][0] + ArrNum[i][1]);
+        }
     }
 }
