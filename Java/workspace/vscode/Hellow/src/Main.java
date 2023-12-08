9537315dabf0d22580eab28e2149cdb79e76b327
ujin302 <ujin302@gmail.com>
2023-12-08 16:35:04 +0900

    백준 1차원 배열 5597

--- Java/workspace/vscode/Hellow/src/Main.java
+++ Java/workspace/vscode/Hellow/src/Main.java
@@ -2,36 +2,22 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        // n == 바구니 개수
-        // m == 시도 횟수
-
         Scanner sc = new Scanner(System.in);
-        String nm = sc.nextLine(); // 바구니 최대 번호, 시도 횟수
+        // 학생 30명 중에 28명만 과제 제출
+        // 과제 제출하지 않은 학생 2명의 학번은?
 
-        int[] baskeNum = new int[Integer.parseInt(nm.split(" ")[0])];
+        // int[] Submit = new int[28]; // 제출한 28명 학생
+        String[] sutdent = new String[30]; // 30명 학생 확인
         String output = "";
-
-        // 바구니 안의 공 번호 세팅
-        for (int i = 0; i < Integer.parseInt(nm.split(" ")[0]); i++) {
-            baskeNum[i] = i + 1;
+        for (int i = 0; i < 28; i++) {
+            int sutdentNum = Integer.parseInt(sc.nextLine());
+            sutdent[sutdentNum - 1] = "O";
         }
-
-        // 바구니 안의 공 교환
-        for (int i = 0; i < Integer.parseInt(nm.split(" ")[1]); i++) {
-            String inputChange = sc.nextLine(); // 교환 번호
-            int start = Integer.parseInt(inputChange.split(" ")[0]); // 교환 시작
-            int end = Integer.parseInt(inputChange.split(" ")[1]); // 교환 종료
-            int temp = 0;
-
-            temp = baskeNum[start - 1];
-            baskeNum[start - 1] = baskeNum[end - 1];
-            baskeNum[end - 1] = temp;
+        for (int i = 0; i < 30; i++) {
+            if (sutdent[i] != "O") {
+                output += (i + 1) + "\n";
+            }
         }
-
-        for (int i = 0; i < baskeNum.length; i++) {
-            output += baskeNum[i] + " ";
-        }
-
         System.out.println(output);
     }
 }
