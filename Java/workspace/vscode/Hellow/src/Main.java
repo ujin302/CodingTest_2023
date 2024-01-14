c0b16feab97d76da2ce281b8065bc85dfaf86eed
유진 <82002058+ujin302@users.noreply.github.com>
2024-01-14 19:01:15 +0900

    프로그래머스

--- Java/workspace/vscode/Hellow/src/Main.java
+++ Java/workspace/vscode/Hellow/src/Main.java
@@ -1,31 +1,52 @@
+import java.util.Dictionary;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int n = scanner.nextInt(); // 시험장 개수
-        // ... int 범위 생각해보기... 데이터 저장할 때 타입 고려하기...!
-        long result = n; // 총감독관 각 시험장에 한명 씩 배정
-        int[] a = new int[n]; // 각 시험장별 응시자 수
+    public static int[] solution(String[] name, int[] yearning, String[][] photo) {
+        int[] answer = {};
+        answer = new int[photo.length];
+        // name 사진 속 인물
+        // yearing 그리움 점수
+        // photo 구해야할 각 사진의 인물들
 
-        // 각 시험장별 응시자 수 설정
-        for (int i = 0; i < n; i++) {
-            a[i] = scanner.nextInt();
-        }
-
-        int b = scanner.nextInt(); // 총감독관 한 시험장에서 감시할 수 있는 응시자 수
-        int c = scanner.nextInt(); // 부감독관 한 시험장에서 감시할 수 있는 응시자 수
-
-        // 시험장별 총감독관은 오직 1명 부감독관은 1명 이상
-        for (int i = 0; i < n; i++) {
-            a[i] -= b; // 총감독관이 감시하는 응시자 제외하기
-            if (a[i] > 0) {
-                result += a[i] / c;
-                if (a[i] % c != 0) {
-                    result++;
+        for (int i = 0; i < photo.length; i++) {
+            for (int j = 0; j < photo[i].length; j++) {
+                for (int n = 0; n < name.length; n++) {
+                    if (photo[i][j].equals(name[n])) {
+                        // System.out.print(name[n]);
+                        // System.out.print(yearning[n]);
+                        answer[i] += yearning[n];
+                        // System.out.print(answer[i]);
+                    } else
+                        continue;
                 }
             }
         }
-        System.out.println(result);
+        return answer;
+
+    }
+
+    public static void main(String[] args) {
+        String[] name = { "kali", "mari", "don" };
+        int[] year = { 11, 1, 55 };
+        String[][] photo = {
+                { "kali", "mari", "don" },
+                { "pony", "tom", "teddy" },
+                { "con", "mona", "don" }
+        };
+
+        int[] str = solution(name, year, photo);
+        System.out.println(str[0]);
+        System.out.println(str[1]);
+
+        System.out.println(str[2]);
+        // String a = solution("abc12345", "ABC", 2);
+        // System.out.println(a);
+
+        // a = solution("Program29b8UYP", "merS123", 7);
+        // System.out.println(a);
+
+        // a = solution("He11oWor1d", "lloWorl", 2);
+        // System.out.println(a);
     }
 }
