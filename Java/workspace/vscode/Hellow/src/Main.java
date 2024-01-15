9348c3f7337202a2a1d87ca910ad7b658de3d874
유진 <82002058+ujin302@users.noreply.github.com>
2024-01-15 23:24:39 +0900

    달리기 경주

--- Java/workspace/vscode/Hellow/src/Main.java
+++ Java/workspace/vscode/Hellow/src/Main.java
@@ -1,53 +1,16 @@
-import java.util.Dictionary;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 public class Main {
-    public static int[] solution(String[] name, int[] yearning, String[][] photo) {
-        System.out.println("추억의 문제");
-        int[] answer = {};
-        answer = new int[photo.length];
-        // name 사진 속 인물
-        // yearing 그리움 점수
-        // photo 구해야할 각 사진의 인물들
-
-        for (int i = 0; i < photo.length; i++) {
-            for (int j = 0; j < photo[i].length; j++) {
-                for (int n = 0; n < name.length; n++) {
-                    if (photo[i][j].equals(name[n])) {
-                        // System.out.print(name[n]);
-                        // System.out.print(yearning[n]);
-                        answer[i] += yearning[n];
-                        // System.out.print(answer[i]);
-                    } else
-                        continue;
-                }
-            }
-        }
-        return answer;
-
+    public static String[] solutionMap(String[] players, String[] callings) {
+        return players;
     }
 
     public static void main(String[] args) {
-        String[] name = { "kali", "mari", "don" };
-        int[] year = { 11, 1, 55 };
-        String[][] photo = {
-                { "kali", "mari", "don" },
-                { "pony", "tom", "teddy" },
-                { "con", "mona", "don" }
-        };
 
-        int[] str = solution(name, year, photo);
-        System.out.println(str[0]);
-        System.out.println(str[1]);
-
-        System.out.println(str[2]);
-        // String a = solution("abc12345", "ABC", 2);
-        // System.out.println(a);
-
-        // a = solution("Program29b8UYP", "merS123", 7);
-        // System.out.println(a);
-
-        // a = solution("He11oWor1d", "lloWorl", 2);
-        // System.out.println(a);
     }
 }
