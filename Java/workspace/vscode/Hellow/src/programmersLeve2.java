8ac7e6b098ffe464037b6507a8844888d6506e3e
유진 <82002058+ujin302@users.noreply.github.com>
2024-03-12 15:00:22 +0900

    정수 삼각형

--- Java/workspace/vscode/Hellow/src/programmersLeve2.java
+++ Java/workspace/vscode/Hellow/src/programmersLeve2.java
@@ -1,68 +1,82 @@
 import java.util.HashMap;
-import java.util.ArrayList;
 import java.util.Collections;
+import java.util.ArrayList;
 
 public class programmersLeve2 {
-    public int solution(int[][] triangle) {
-        int answer = 0;
+
+    public static int solution(int[][] triangle) {
+        System.out.println("정수 삼각형");
+
+        int len = triangle.length;
         // 1. dp 초기화
-        int[][] dp = new int[triangle.length][triangle.length];
+        int[][] dp = new int[len][len];
         dp[0][0] = triangle[0][0];
+        // 1-1. 첫번째 요소 초기화 [i][0]
+        for (int i = 1; i < len; i++) {
+            dp[i][0] = triangle[i][0] + dp[i - 1][0];
+        }
 
         // 2. 동적 계획법
-        for (int i = 1; i < triangle.length; i++) {
-            for (int j = i; j < triangle.length; j++) {
-
+        // 2-2. 첫번째 요소를 제외한 모든 값 초기화
+        for (int i = 1; i < len; i++) {
+            for (int j = 1; j < i + 1; j++) {
+                dp[i][j] = triangle[i][j] + Math.max(dp[i - 1][j - 1], dp[i - 1][j]);
             }
+        }
+
+        // 3. 최댓값 구하기
+        int max = 0;
+        for (int i = 0; i < len; i++) {
+            max = Math.max(dp[len - 1][i], max);
+        }
+
+        return max;
+    }
+
+    public static int s1(int k, int[] tangerine) {
+        System.out.println("귤 고르기");
+        int answer = 0;
+        HashMap<Integer, Integer> mappedSize = new HashMap<>();
+
+        // Map<귤 크기, 개수> 초기화
+        // getOrDefault 메소드 : 찾는 Key 값이 없을 경우, 두번째 인수값으로 설정됨.
+        for (int i = 0; i < tangerine.length; i++) {
+            mappedSize.put(tangerine[i], mappedSize.getOrDefault(tangerine[i], 0) + 1);
+            // 귤 크기, 0
+        }
+
+        // // Map<귤 크기, 0> 초기화
+        // for (int i = 0; i < tangerine.length; i++) {
+        // mappedSize.put(tangerine[i], 0); // 귤 크기, 0
+        // }
+        // // Map<귤 크기, 개수>
+        // for (int key : mappedSize.keySet()) {
+        // for (int i = 0; i < tangerine.length; i++) {
+        // if (key == tangerine[i]) {
+        // int temp = mappedSize.get(key) + 1;
+        // mappedSize.put(key, temp);
+        // }
+        // }
+        // }
+
+        // Value 값들을 ArrayList에 저장
+        ArrayList<Integer> valuArrayList = new ArrayList<>(mappedSize.values());
+        // 내림차순 정렬
+        Collections.sort(valuArrayList, Collections.reverseOrder());
+        // 다른 종류 개수 찾아내기
+        for (int value : valuArrayList) {
+            if (k <= 0) {
+                break;
+            } else
+                answer++;
+            k -= value;
         }
 
         return answer;
     }
 
-    // public static int s1(int k, int[] tangerine) {
-    // System.out.println("귤 고르기");
-    // int answer = 0;
-    // HashMap<Integer, Integer> mappedSize = new HashMap<>();
-
-    // // Map<귤 크기, 개수> 초기화
-    // // getOrDefault 메소드 : 찾는 Key 값이 없을 경우, 두번째 인수값으로 설정됨.
-    // for (int i = 0; i < tangerine.length; i++) {
-    // mappedSize.put(tangerine[i], mappedSize.getOrDefault(tangerine[i], 0) + 1);
-    // // 귤 크기, 0
-    // }
-
-    // // // Map<귤 크기, 0> 초기화
-    // // for (int i = 0; i < tangerine.length; i++) {
-    // // mappedSize.put(tangerine[i], 0); // 귤 크기, 0
-    // // }
-    // // // Map<귤 크기, 개수>
-    // // for (int key : mappedSize.keySet()) {
-    // // for (int i = 0; i < tangerine.length; i++) {
-    // // if (key == tangerine[i]) {
-    // // int temp = mappedSize.get(key) + 1;
-    // // mappedSize.put(key, temp);
-    // // }
-    // // }
-    // // }
-
-    // // Value 값들을 ArrayList에 저장
-    // ArrayList<Integer> valuArrayList = new ArrayList<>(mappedSize.values());
-    // // 내림차순 정렬
-    // Collections.sort(valuArrayList, Collections.reverseOrder());
-    // // 다른 종류 개수 찾아내기
-    // for (int value : valuArrayList) {
-    // if (k <= 0) {
-    // break;
-    // } else
-    // answer++;
-    // k -= value;
-    // }
-
-    // return answer;
-    // }
-
     public static void main(String[] args) {
-        int[] tangerine1 = { 1, 3, 2, 5, 4, 5, 2, 3 };
-        System.out.println(solution(1, tangerine1));
+        int[][] tangerine1 = { { 7 }, { 3, 8 }, { 8, 1, 0 }, { 2, 7, 4, 4 }, { 4, 5, 2, 6, 5 } };
+        System.out.println(solution(tangerine1));
     }
 }
