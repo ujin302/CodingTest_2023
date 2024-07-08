b830df35e993026687537663ca5912319fd3c29b
유진 <82002058+ujin302@users.noreply.github.com>
2024-07-08 14:43:14 +0900

    Lv2. 공원 산책

--- Java/workspace/vscode/Hellow/src/programmers/programmersLeve1.java
+++ Java/workspace/vscode/Hellow/src/programmers/programmersLeve1.java
@@ -1,7 +1,110 @@
 import java.util.*;
 
-// test
 public class programmersLeve1 {
+    public static int[] solution5(String[] park, String[] routes) {
+        System.out.println("공원 산책");
+        /*
+         * S : 시작
+         * O : 이동 가능
+         * X : 장애물
+         * 
+         * N : 위
+         * S : 아래
+         * E : 앞으로
+         * W : 뒤로
+         */
+
+        int h = park.length, w = park[0].length();
+        int x = 0, y = 0;
+        String[][] parkArr = new String[h][w];
+
+        // 1. 공원 2차원배열에 저장
+        for (int i = 0; i < h; i++) {
+            for (int j = 0; j < w; j++) {
+                parkArr[i][j] = park[i].toCharArray()[j] + "";
+                if (parkArr[i][j].equals("S")) {
+                    x = i;
+                    y = j;
+                }
+            }
+        }
+
+        // 2. 이동
+        for (String str : routes) {
+            int tempX = x, tempY = y; // 현재 위치
+            String move = str.split(" ")[0]; // 방향
+            int count = Integer.parseInt(str.split(" ")[1]); // 이동 칸 수
+
+            for (int i = 0; i < count; i++) {
+                // 방향과 이동 칸 수 확인
+                switch (move) {
+                    case "N": // 위로
+                        tempX--;
+                        break;
+                    case "S": // 아래로
+                        tempX++;
+                        break;
+                    case "E": // 앞으로
+                        tempY++;
+                        break;
+                    case "W": // 뒤로
+                        tempY--;
+                        break;
+                }
+
+                if ((tempX >= 0 && tempX < h) && (tempY >= 0 && tempY < w)) {
+                    // 범위 안에 있음
+                    if (parkArr[tempX][tempY].equals("X")) {
+                        // 장애물 만남
+                        break;
+                    }
+
+                    if (i == count - 1) {
+                        x = tempX;
+                        y = tempY;
+                    }
+                }
+
+            }
+        }
+
+        return new int[] { x, y };
+    }
+
+    public static int[] solution4_1(String[] id_list, String[] report, int k) {
+        System.out.println("신고 결과 받기");
+        int listLen = id_list.length;
+        HashMap<String, Integer> indexMap = new HashMap<String, Integer>(); // 식별번호
+        HashMap<String, HashSet<String>> reportMap = new HashMap<String, HashSet<String>>(); // 신고당한 사람, 신고한 사람
+        int[] mailCountArr = new int[listLen]; // 메일 전송 개수 저장 배열
+
+        // 1. 사람별 index 설정
+        for (int i = 0; i < listLen; i++) {
+            indexMap.put(id_list[i], i);
+        }
+
+        // 2. 신고 정보 설정
+        for (int i = 0; i < report.length; i++) {
+            String a = report[i].split(" ")[0]; // 신고한 사람
+            String b = report[i].split(" ")[1]; // 신고 당한 사람
+
+            // 신고당한 사람 - 신고한 사람
+            reportMap.put(b, reportMap.getOrDefault(b, new HashSet<>())); // 초기화
+            reportMap.get(b).add(a);
+        }
+
+        // 4. 메일 전송
+        for (String stopName : reportMap.keySet()) {
+            if (reportMap.get(stopName).size() >= k) { // 중단 계정 선별
+                for (String name : reportMap.get(stopName)) {
+                    mailCountArr[indexMap.get(name)]++;
+                }
+            }
+        }
+
+        return mailCountArr;
+    }
+
     public static int[] solution4(String[] id_list, String[] report, int k) {
         System.out.println("신고 결과 받기");
         /*
@@ -14,10 +117,8 @@ public class programmersLeve1 {
         int listLen = id_list.length;
         HashMap<String, Integer> indexMap = new HashMap<String, Integer>(); // 식별번호
         String[][] reportArr = new String[listLen][report.length]; // 신고한 명단 배열
-        // int[][] reportArr = new int[listLen][report.length]; // 신고한 명단 배열
         int[] getReportArr = new int[listLen]; // 신고당한 횟수 배열
         int[] mailCountArr = new int[listLen]; // 메일 전송 개수 저장 배열
-        // int[] stopNameArr = new int[listLen]; // 정지 당한 명단 배열
         String[] stopNameArr = new String[listLen]; // 정지 당한 명단 배열
 
         int num = 0;
@@ -29,9 +130,6 @@ public class programmersLeve1 {
 
         // 2. 신고당한 횟수 배열 && 신고한 명단 배열
         for (int i = 0; i < report.length; i++) {
-            // int a = indexMap.get(report[i].split(" ")[0]); // 신고한 사람
-            // int b = indexMap.get(report[i].split(" ")[1]); // 신고 당한 사람
-
             String a = report[i].split(" ")[0]; // 신고한 사람
             String b = report[i].split(" ")[1]; // 신고 당한 사람
 
@@ -80,19 +178,6 @@ public class programmersLeve1 {
                 }
             }
         }
-
-        // for (int i = 0; i < listLen; i++) {
-        // for (int t = 0; t < report.length; t++) {
-        // if (reportArr[i][t] == 0)
-        // break;
-
-        // for (int intNum : stopNameArr) {
-        // if (reportArr[i][t] == intNum) {
-        // mailCountArr[i]++;
-        // }
-        // }
-        // }
-        // }
 
         return mailCountArr;
     }
@@ -217,14 +302,14 @@ public class programmersLeve1 {
     }
 
     public static void main(String[] args) {
-        String[] id_list = { "muzi", "frodo", "apeach", "neo" };
-        String[] report = { "muzi frodo", "apeach frodo", "frodo neo", "muzi neo",
-                "apeach muzi" };
+        // String[] park = { "SOO", "OOO", "OOO" };
+        // String[] routes = { "E 2", "S 2", "W 1" };
 
-        // String[] id_list = { "con", "ryan" };
-        // String[] report = { "ryan con", "ryan con", "ryan con", "ryan con" };
-        int k = 2;
+        // String[] park = { "SOO", "OXX", "OOO" };
+        // String[] routes = { "E 2", "S 2", "W 1" };
 
-        solution4(id_list, report, k);
+        String[] park = { "OSO", "OOO", "OXO", "OOO" };
+        String[] routes = { "E 2", "S 3", "W 1" };
+        solution5(park, routes);
     }
 }
