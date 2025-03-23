af76a1a6a7b1dadba81b949af657ae7c1572a92c
ujin302 <ujin302@gmail.com>
2025-03-23 23:23:19 +0900

    백준 Bronze1: 2775. 부녀회장이 될테야

--- Java/workspace/vscode/Hellow/src/BackJun/bronze1.java
+++ Java/workspace/vscode/Hellow/src/BackJun/bronze1.java
@@ -34,8 +34,46 @@ public class bronze1 {
         }
     }
 
+    private static int[][] aptFill(int[][] apt, int downk, int n) {
+        // '2775. 부녀회장이 될테야' 에서 필요함수.
+        if (apt[downk][n] == 0) {
+            apt = aptFill(apt, downk - 1, n);
+        }
+
+        for (int i = 2; i <= n; i++) {
+            if (apt[downk + 1][i] == 0) {
+                for (int j = 1; j <= i; j++) {
+                    apt[downk + 1][i] += apt[downk][j];
+                }
+            }
+        }
+
+        return apt;
+    }
+
+    public void s2775() {
+        // 2775. 부녀회장이 될테야
+        Scanner sc = new Scanner(System.in);
+        int tc = sc.nextInt();
+        int[][] apt = new int[15][15];
+        for (int i = 0; i < 15; i++) {
+            apt[0][i] = i;
+            apt[i][1] = 1;
+        }
+
+        for (int i = 0; i < tc; i++) {
+            int k = sc.nextInt(); // 층
+            int n = sc.nextInt(); // 호
+
+            if (apt[k][n] == 0) {
+                apt = aptFill(apt, k - 1, n);
+            }
+
+            System.out.println(apt[k][n]);
+        }
+    }
+
     public static void main(String[] args) {
-        
 
     }
 }
