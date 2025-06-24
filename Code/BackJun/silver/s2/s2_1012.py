f6565792d40e73d1cb642b6f00684615abdf5666
ujin302 <ujin302@gmail.com>
2025-06-24 16:43:47 +0900

    [Python] 백준 Silver2: 1012.유기농 배추 ver2.Queue 사용

--- Code/BackJun/silver/s2/s2_1012.py
+++ Code/BackJun/silver/s2/s2_1012.py
@@ -1,5 +1,6 @@
 import sys;
-sys.setrecursionlimit(10**5)
+import queue;
+# sys.setrecursionlimit(10**5) # findEarthworm() 함수 사용 시, 필요
 
 def findEarthworm(startY, startX):
     checkArr[startY][startX] = True
@@ -21,6 +22,32 @@ def findEarthworm(startY, startX):
             findEarthworm(moveY, moveX)
             
 
+def useQueue(startY, startX):
+    nearNode = queue.Queue()
+    checkArr[startY][startX] = True
+    nearNode.put([startY, startX])
+    
+    # 앞뒤위아래
+    moveXArr = [1, -1, 0, 0]
+    moveYArr = [0, 0, -1, 1]
+    
+    while nearNode.qsize() > 0:
+        node = nearNode.get()
+        
+        for i in range(4):
+            moveX = node[1] + moveXArr[i]
+            moveY = node[0] + moveYArr[i]
+            
+            if moveX < 0 or moveY < 0:
+                continue
+            if moveX >= m or moveY >= n:
+                continue
+            
+            if targetArr[moveY][moveX] == 1 and not checkArr[moveY][moveX]:
+                checkArr[moveY][moveX] = True
+                nearNode.put([moveY, moveX])
+            
+
 tc = int(input())
 
 for i in range(tc):
@@ -39,6 +66,7 @@ for i in range(tc):
     for y in range(n):
         for x in range(m):
             if targetArr[y][x] == 1 and not checkArr[y][x]:
-                findEarthworm(y, x)
+                # findEarthworm(y, x)
+                useQueue(y, x)
                 result+=1
     print(result)
